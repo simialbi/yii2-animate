92ef0ea74f4c489694097ea66f5676a25f4d09df
simialbi <simi.albi@gmail.com>
2018-09-13 17:40:33 +0200

    check if in view before trigger animation

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,29 +1,47 @@
 /*globals jQuery,yii,window,moment,JSON:false */
 (function ($, window, undefined) {
+	var animationEnd = (function (el) {
+		var animations = {
+			animation: 'animationend',
+			OAnimation: 'oAnimationEnd',
+			MozAnimation: 'mozAnimationEnd',
+			WebkitAnimation: 'webkitAnimationEnd'
+		};
+
+		for (var t in animations) {
+			if (el.style[t] !== undefined) {
+				return animations[t];
+			}
+		}
+	})(document.createElement('div'));
+
 	$.fn.extend({
 		animateCss: function (animationName, callback) {
-			var animationEnd = (function (el) {
-				var animations = {
-					animation: 'animationend',
-					OAnimation: 'oAnimationEnd',
-					MozAnimation: 'mozAnimationEnd',
-					WebkitAnimation: 'webkitAnimationEnd'
-				};
-
-				for (var t in animations) {
-					if (el.style[t] !== undefined) {
-						return animations[t];
-					}
-				}
-			})(document.createElement('div'));
-
-			this.addClass('animated ' + animationName).one(animationEnd, function () {
-				$(this).removeClass('animated ' + animationName);
+			this.one(animationEnd, function () {
+				var $this = $(this);
+				$this.removeClass('animated ' + animationName);
+				$this.data('animation', null);
 
 				if (typeof callback === 'function') {
 					callback();
 				}
 			});
+
+			$(window).on('scroll.animation resize.animation lookup.animation', function () {
+				this.filter(function() {
+					var $e = $(this);
+					if ($e.is(":hidden")) return;
+
+					var wt = $w.scrollTop(),
+						wb = wt + $w.height(),
+						et = $e.offset().top,
+						eb = et + $e.height();
+
+					return eb >= wt - th && et <= wb + th;
+				}).addClass('animated ' + animationName);
+			});
+
+			$(window).trigger('scroll.animation');
 
 			return this;
 		}
@@ -31,6 +49,7 @@
 
 	$('[data-animation]').each(function () {
 		var $this = $(this);
+
 		$this.animateCss($this.data('animation'));
 	});
 })(jQuery, window);
